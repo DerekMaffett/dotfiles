1b62d767ffaf95440e106703e68706f7f34a709b
Derek Maffett <derekgmaffett@gmail.com>
2018-11-24 23:50:15 +0100

    add nvm/npm to installation scripts

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -7,3 +7,11 @@ brew install cloc
 # Vim-plug package manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Installs nvm, sets node version, and implicitly installs npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.nvm/nvm.sh # necessary to access nvm command
+nvm install v11.2.0
+
+echo "The following commands may require restarting your terminal..."
+npm install -g elm-format
--- vimrc
+++ vimrc
@@ -4,12 +4,16 @@ Plug 'tpope/vim-surround'
 
 Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
 Plug 'junegunn/fzf.vim'
-
 nnoremap <c-p> :Files<cr>
 
 Plug 'srcery-colors/srcery-vim'
 
 Plug 'tomtom/tcomment_vim'
+
+Plug 'leafgarland/typescript-vim'
+
+Plug 'elmcast/elm-vim'
+let g:elm_format_autosave = 1
 
 call plug#end()
 
