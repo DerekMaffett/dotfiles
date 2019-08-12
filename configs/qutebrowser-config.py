2d60ed9f3126d096379cf96449f84f60cfda6d94
Derek Maffett <derekgmaffett@gmail.com>
2019-08-12 11:34:36 +0200

    fix password script call

--- configs/qutebrowser-config.py
+++ configs/qutebrowser-config.py
@@ -4,7 +4,7 @@ c.tabs.position = "left"
 c.bindings.key_mappings['<Ctrl-c>'] = '<Escape>'
 config.bind('<Ctrl-c>', 'leave-mode', mode="command")
 
-config.bind(',p', 'spawn --userscripts qute-lastpass')
+config.bind(',p', 'spawn --userscript qute-lastpass')
 config.bind(',s', 'config-clear ;; config-source ;; message-info "configuration reloaded!"')
 config.bind(',qq', 'quit')
 config.bind('Ctrl-p', 'set-cmd-text -s :bookmark-load -t');
