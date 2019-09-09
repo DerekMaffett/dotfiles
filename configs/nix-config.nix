82f685a7c16c36a35c2cf68c14ce99a2df4966bd
Derek Maffett <derekgmaffett@gmail.com>
2019-09-10 01:00:18 +0200

    add local cabal run nix fn

--- configs/nix-config.nix
+++ configs/nix-config.nix
@@ -64,8 +64,12 @@ in {
         name = "potato-colors";
         src = fromGithubMaster "potato-colors";
     };
+
     writeWatchScript = { name, src ? ".", exclude ? "//", command }: 
       writeScriptBin name "${fswatch}/bin/fswatch -r -o -l 0.2 -e ${exclude} ${src} | (while read; do ${command}; done)";
+
+    localCabalRun = name: writeScriptBin name "cabal new-run ${name} $@";
+
     all = buildEnv {
       name = "all";
       paths = with pkgs; [
